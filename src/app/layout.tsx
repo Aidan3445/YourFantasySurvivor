c564698dd44880e4892ca76788551ee613490ea0
Aidan Weinberg <102766475+Aidan3445@users.noreply.github.com>
2024-04-27 13:20:50 -0400

    dev4 - auth (#5)
    auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
-import TopNav from "./components/topNav";
+import { ClerkProvider } from "@clerk/nextjs";
+import TopNav from "./_components/topNav";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -20,11 +21,13 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en">
-            <body className={`font-sans ${inter.variable}`}>
-                <TopNav />
-                {children}
-            </body>
-        </html>
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`font-sans ${inter.variable}`}>
+                    <TopNav />
+                    {children}
+                </body>
+            </html>
+        </ClerkProvider>
     );
 }
