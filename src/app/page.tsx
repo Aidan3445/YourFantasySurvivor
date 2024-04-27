4ee2079a2dbd3d828bcaf7496cc9cc739023822b
Aidan Weinberg <102766475+Aidan3445@users.noreply.github.com>
2024-04-27 11:21:29 -0400

    dev3 - force-dynamic (#4)
    force-dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import { db } from "~/server/db";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
     const posts = await db.query.posts.findMany();
 
@@ -41,12 +43,12 @@ export default async function HomePage() {
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
-        </h1>
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
+            >
+             className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+              k
             className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
             href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
+              rget="_blank"
           >
             <h3 className="text-2xl font-bold">First Steps →</h3>
             <div className="text-lg">
