79bf389dc315d1b8dea81c8d1f119748dbf3cf9e
Aidan Weinberg <102766475+Aidan3445@users.noreply.github.com>
2024-04-27 11:09:59 -0400

    Dev2 - setup postgres (#3)
    * init
    
    * Update README.md
    
    * nav and basic layout
    
    * build improvements
    
    * postgres

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,10 +3,10 @@ import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  driver: "pg",
-  dbCredentials: {
-    connectionString: env.DATABASE_URL,
-  },
-  tablesFilter: ["your-survivor-fantasy_*"],
+    schema: "./src/server/db/schema.ts",
+    driver: "pg",
+    dbCredentials: {
+        connectionString: env.POSTGRES_URL,
+    },
+    tablesFilter: ["your-survivor-fantasy_*"],
 } satisfies Config;
