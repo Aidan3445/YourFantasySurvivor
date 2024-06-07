37c8b57c1963854bbb5883a6fc8d7b1b7a07e05a
Aidan Weinberg <aidan3445@gmail.com>
2024-06-07 08:58:00 -0400

    middleware version fix attempt

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,5 +8,5 @@ export default clerkMiddleware((auth, request) => {
 })
 
 export const config = {
-    matcher: ["/((?!.+\\.[\\w]+$|_next|favicon.ico).*)", "/", "/(api|trpc)(.*)"],
+    matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 }
