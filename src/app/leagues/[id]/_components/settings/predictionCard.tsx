ed22a9429bd711493abfe4e694321b0aaa7105b1
AWeinberg-PL <aidan.weinberg@priceline.com>
2024-09-17 13:46:36 -0400

    moble pred desc

--- src/app/leagues/[id]/_components/settings/predictionCard.tsx
+++ src/app/leagues/[id]/_components/settings/predictionCard.tsx
@@ -1,4 +1,6 @@
+'use client';
 import { Info } from 'lucide-react';
+import { useState } from 'react';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '~/app/_components/commonUI/hover';
 import { cn, type ComponentProps } from '~/lib/utils';
 import { type SeasonEventRuleType } from '~/server/db/schema/seasonEvents';
@@ -10,12 +12,14 @@ interface PredictionCardProps extends ComponentProps {
 }
 
 export function PredictionCard({ prediction, parity, className, children }: PredictionCardProps) {
+  const [open, setOpen] = useState(false);
+
   return (
     <div className={cn(parity ? 'bg-b4/70' : 'bg-b3/80', 'p-2 rounded-md', className)}>
       <span className='text-xs inline-flex gap-2 items-center'>
         <p>Predict {prediction.name} {prediction.points != 0 ? `for ${prediction.points} points.` : ''}</p>
-        <HoverCard>
-          <HoverCardTrigger>
+        <HoverCard open={open} onOpenChange={() => setOpen(!open)}>
+          <HoverCardTrigger onClick={() => setOpen(!open)}>
             <Info size={16} />
           </HoverCardTrigger>
           <HoverCardContent>
