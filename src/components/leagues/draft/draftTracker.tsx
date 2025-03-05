91f2f42432cad79c425196d55f966cc1e5921019
Aidan Weinberg <aidanhweinberg@gmail.com>
2025-03-04 18:14:49 -0600

    Update draftTracker.tsx

--- src/components/leagues/draft/draftTracker.tsx
+++ src/components/leagues/draft/draftTracker.tsx
@@ -99,7 +99,7 @@ export default function DraftTracker({ leagueHash }: DraftTrackerProps) {
               )}
               {pick.draftPick && (
                 <h3
-                  className='text-lg self-end text-wrap'
+                  className='text-lg self-end text-wrap leading-tight'
                   style={{ color: getContrastingColor(pick.color) }}>
                   {pick.draftPick}
                 </h3>
