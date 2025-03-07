a4f5ad058fa63e14ffba853cd0c968ebcbd3ec02
Aidan Weinberg <aidanhweinberg@gmail.com>
2025-03-06 18:15:23 -0600

    Update predictions.tsx

--- src/components/leagues/main/predictions.tsx
+++ src/components/leagues/main/predictions.tsx
@@ -18,8 +18,10 @@ export default async function Predictions({ leagueHash }: PredictionsProps) {
   return (
     <div className='text-center bg-card rounded-lg w-full'>
       <h1 className='text-3xl'>{'This Week\'s Prediction'}{predictions.length > 1 ? 's!' : '!'}</h1>
-      <span className='flex justify-center items-center gap-2 text-muted-foreground text-sm'>
-        {nextEpisode.episodeNumber}: {nextEpisode.episodeTitle}
+      <span className='flex flex-wrap justify-center items-center gap-2 text-muted-foreground text-sm'>
+        <span classname='text-nowrap'>
+          {nextEpisode.episodeNumber}: {nextEpisode.episodeTitle}
+        </span>
         <AirStatus airDate={nextEpisode.episodeAirDate} airStatus={nextEpisode.airStatus} />
       </span>
       <PredictionCards
