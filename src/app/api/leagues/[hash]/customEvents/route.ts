683b162eea62fc146b694e66f6725c7b86da292a
Aidan Weinberg <aidan3445@gmail.com>
2025-09-02 17:27:27 -0400

    get custom events and predictions together

--- src/app/api/leagues/[hash]/customEvents/route.ts
+++ src/app/api/leagues/[hash]/customEvents/route.ts
@@ -3,14 +3,14 @@ import 'server-only';
 import { type NextRequest, NextResponse } from 'next/server';
 import type { LeagueRouteParams } from '~/types/api';
 import { withLeagueAuth } from '~/lib/api-middleware';
-import getCustomEvents from '~/services/leagues/query/customEvents';
+import getCustomEventsAndPredictions from '~/services/leagues/query/customEvents';
 
 export async function GET(request: NextRequest, context: LeagueRouteParams) {
   return withLeagueAuth(async (_, context, __) => {
     const { hash } = await context.params;
     try {
-      const customEvents = await getCustomEvents(hash);
-      return NextResponse.json({ customEvents }, { status: 200 });
+      const customEventsAndPredictions = await getCustomEventsAndPredictions(hash);
+      return NextResponse.json({ ...customEventsAndPredictions }, { status: 200 });
     } catch (e) {
       console.error(e);
       return NextResponse.json({ error: (e as Error).message }, { status: 500 });
--- src/services/leagues/query/customEvents.ts
+++ src/services/leagues/query/customEvents.ts
@@ -1,11 +1,26 @@
 import 'server-only';
 
 import { db } from '~/server/db';
-import { eq } from 'drizzle-orm';
+import { and, eq, sql } from 'drizzle-orm';
 import { leagueSchema } from '~/server/db/schema/leagues';
 import { episodeSchema } from '~/server/db/schema/episodes';
-import { customEventReferenceSchema, customEventRuleSchema, customEventSchema } from '~/server/db/schema/customEvents';
-import { type EventWithReferences } from '~/types/events';
+import { customEventPredictionSchema, customEventReferenceSchema, customEventRuleSchema, customEventSchema } from '~/server/db/schema/customEvents';
+import { type EventWithReferences, type Prediction } from '~/types/events';
+import { leagueMemberSchema } from '~/server/db/schema/leagueMembers';
+
+/**
+  * Get custom events and predictions for a league
+  * @param hash The hash of the league
+  * @returns the custom league events and predictions
+  * @returnObj `events: Record<episodeNumber, Record<eventId, EventWithReferences>>
+  * predictions: Prediction[]`
+  */
+export default async function getCustomEventsAndPredictions(hash: string) {
+  const eventsReq = getCustomEvents(hash);
+  const predictionsReq = getCustomPredictions(hash);
+  const [events, predictions] = await Promise.all([eventsReq, predictionsReq]);
+  return { events, predictions };
+}
 
 /**
   * Get the custom events for a league
@@ -13,7 +28,7 @@ import { type EventWithReferences } from '~/types/events';
   * @returns the custom league events
   * @returnObj `Record<episodeNumber, Record<eventId, EventWithReferences>>`
   */
-export default async function getCustomEvents(hash: string) {
+export async function getCustomEvents(hash: string) {
   return db
     .select({
       episodeNumber: episodeSchema.episodeNumber,
@@ -52,3 +67,45 @@ export default async function getCustomEvents(hash: string) {
       return acc;
     }, {} as Record<number, Record<number, EventWithReferences>>));
 }
+
+/**
+  * Get the custom events for a league
+  * @param hash The hash of the league
+  * @returns the custom league events
+  * @returnObj `Prediction[]`
+  */
+export async function getCustomPredictions(hash: string) {
+  return db
+    .select({
+      eventId: customEventSchema.customEventRuleId,
+      episodeNumber: episodeSchema.episodeNumber,
+      predictionMakerId: customEventPredictionSchema.memberId,
+      referenceId: customEventPredictionSchema.referenceId,
+      referenceType: customEventPredictionSchema.refernceType,
+      hit: sql<boolean>`
+        CASE WHEN ${customEventReferenceSchema} = ${customEventPredictionSchema.referenceId}
+        AND ${customEventReferenceSchema} = ${customEventPredictionSchema.refernceType}
+        THEN true ELSE false END
+      `.as('hit'),
+    })
+    .from(customEventPredictionSchema)
+    .innerJoin(episodeSchema, eq(episodeSchema.episodeId, customEventPredictionSchema.episodeId))
+    .innerJoin(leagueMemberSchema, eq(leagueMemberSchema.memberId, customEventPredictionSchema.memberId))
+    .innerJoin(leagueSchema, eq(leagueSchema.leagueId, leagueMemberSchema.leagueId))
+    // result
+    .innerJoin(customEventSchema, and(
+      eq(customEventSchema.customEventRuleId, customEventPredictionSchema.customEventRuleId),
+      eq(customEventSchema.episodeId, episodeSchema.episodeId)))
+    .innerJoin(customEventRuleSchema, and(
+      eq(customEventRuleSchema.customEventRuleId, customEventSchema.customEventRuleId),
+      eq(customEventRuleSchema.eventType, 'Prediction')))
+    .leftJoin(customEventReferenceSchema, eq(customEventReferenceSchema.customEventId, customEventSchema.customEventId))
+    .where(eq(leagueSchema.hash, hash))
+    .orderBy(episodeSchema.episodeNumber)
+    .then(predictions => predictions.map(p => ({
+      ...p,
+      eventSource: 'Custom'
+    } as Prediction)));
+}
+
+
