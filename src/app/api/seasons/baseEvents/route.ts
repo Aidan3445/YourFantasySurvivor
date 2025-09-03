3dfe1e5e68756320d620f55130accdfa11712c52
Aidan Weinberg <aidan3445@gmail.com>
2025-09-03 12:08:44 -0400

    simplify routing for baseEvents endpoints

--- src/app/api/seasons/baseEvents/route.ts
+++ src/app/api/seasons/baseEvents/route.ts
@@ -1,5 +1,10 @@
 import { type NextRequest, NextResponse } from 'next/server';
+import { requireSystemAdminAuth } from '~/lib/auth';
 import getBaseEvents from '~/services/seasons/query/baseEvents';
+import createBaseEventLogic from '~/services/seasons/mutation/createBaseEvent';
+import deleteBaseEventLogic from '~/services/seasons/mutation/deleteBaseEvent';
+import updateBaseEventLogic from '~/services/seasons/mutation/updateBaseEvent';
+import { type BaseEventInsert } from '~/types/events';
 
 export async function GET(req: NextRequest) {
   const seasonIdParam = req.nextUrl.searchParams.get('seasonId');
@@ -15,5 +20,59 @@ export async function GET(req: NextRequest) {
   } catch (e) {
     console.error(e);
     return NextResponse.json({ error: (e as Error).message }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const baseEvent = await request.json() as BaseEventInsert;
+    const newEventId = await requireSystemAdminAuth(createBaseEventLogic)(baseEvent);
+    return NextResponse.json({ newEventId }, { status: 200 });
+  } catch (error) {
+    let message: string;
+    if (error instanceof Error) message = error.message;
+    else message = String(error);
+
+    if (message === 'User not authorized') {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
+
+export async function PUT(request: NextRequest) {
+  try {
+    const { baseEventId, baseEvent } = await request.json() as {
+      baseEventId: number,
+      baseEvent: BaseEventInsert
+    };
+    const { success } = await requireSystemAdminAuth(updateBaseEventLogic)(baseEventId, baseEvent);
+    return NextResponse.json({ success }, { status: 200 });
+  } catch (error) {
+    let message: string;
+    if (error instanceof Error) message = error.message;
+    else message = String(error);
+
+    if (message === 'User not authorized') {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
+  }
+}
+
+export async function DELETE(request: NextRequest) {
+  try {
+    const { baseEventId } = await request.json() as { baseEventId: number };
+    const { success } = await requireSystemAdminAuth(deleteBaseEventLogic)(baseEventId);
+    return NextResponse.json({ success }, { status: 200 });
+  } catch (error) {
+    let message: string;
+    if (error instanceof Error) message = error.message;
+    else message = String(error);
+
+    if (message === 'User not authorized') {
+      return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+    }
+    return NextResponse.json({ error: 'Server error' }, { status: 500 });
   }
 }
