837662297c4dd6ddaee4019f484fd993ece22c40
Aidan Weinberg <aidan3445@gmail.com>
2025-09-04 10:32:20 -0400

    shauhinmode service

---
+++ src/actions/updateShauhinMode.ts
@@ -0,0 +1,32 @@
+'use server';
+
+import { requireLeagueMemberAuth } from '~/lib/auth';
+import updateShauhinModeLogic from '~/services/leagues/mutation/updateShauhinMode';
+import { type ShauhinModeSettings } from '~/types/leagues';
+
+/**
+  * Update the Shauhin Mode settings for a league
+  * @param leagueHash The hash of the league
+  * @param shauhinMode The new Shauhin Mode settings
+  * @throws an error if the Shauhin Mode settings cannot be updated
+  * @throws an error if the league is inactive
+  * @returns Success status of the update
+  * @returnObj `{ success }`
+  */
+export default async function updateShauhinMode(
+  leagueHash: string,
+  shauhinMode: ShauhinModeSettings,
+) {
+  try {
+    return await requireLeagueMemberAuth(updateShauhinModeLogic)(leagueHash, shauhinMode);
+  } catch (e) {
+    let message: string;
+    if (e instanceof Error) message = e.message;
+    else message = String(e);
+
+    if (message.includes('User not') || message.includes('Not a league member')) throw e;
+
+    console.error('Failed to update Shauhin Mode settings', e);
+    throw new Error('An error occurred while updating the Shauhin Mode settings.');
+  }
+}
