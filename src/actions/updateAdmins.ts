1779e3ab46dc2ed0b993a458b4fd4653bb0b6d92
Aidan Weinberg <aidan3445@gmail.com>
2025-09-04 01:48:19 -0400

    update admins services

---
+++ src/actions/updateAdmins.ts
@@ -0,0 +1,28 @@
+'use server';
+
+import { requireLeagueMemberAuth } from '~/lib/auth';
+import updateAdminsLogic from '~/services/leagues/mutation/updateAdmins';
+import { type LeagueHash } from '~/types/deprecated/leagues';
+
+/**
+ * Update league admin list
+ * @param auth The authenticated league member
+ * @param admins The new list of admins
+ * @throws an error if the admins cannot be updated
+ * @returns Success status of the update
+ * @returnObj `{ success }`
+ */
+export default async function updateAdmins(leagueHash: LeagueHash, admins: number[]) {
+  try {
+    return await requireLeagueMemberAuth(updateAdminsLogic)(leagueHash, admins);
+  } catch (e) {
+    let message: string;
+    if (e instanceof Error) message = e.message;
+    else message = String(e);
+
+    if (message.includes('User not') || message.includes('Not a league member')) throw e;
+
+    console.error('Failed to update admins', e);
+    throw new Error('An error occurred while updating the admins.');
+  }
+}
