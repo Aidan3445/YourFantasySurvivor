f6dc257ef48e97979e94640eee51e164b28b545d
Aidan Weinberg <aidan3445@gmail.com>
2025-09-28 18:14:02 -0400

    img bc no cache

--- src/components/nav/side/footer.tsx
+++ src/components/nav/side/footer.tsx
@@ -1,8 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 import { ClerkLoaded, ClerkLoading, SignedIn, UserButton } from '@clerk/nextjs';
 import { SidebarMenuButton, SidebarMenu } from '~/components/common/sidebar';
 import { LoaderCircle } from 'lucide-react';
 import Link from 'next/link';
-import Image from 'next/image';
 
 export default function SideNavFooter() {
   return (
@@ -12,7 +12,7 @@ export default function SideNavFooter() {
         asChild
         size='lg'>
         <Link href='https://www.buymeacoffee.com/aidanweinberg' target='_blank' rel='noreferrer'>
-          <Image
+          <img
             className='hover:scale-105 active:scale-[1.075] transition-all'
             src={'https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=aidanweinberg&button_colour=40DCA5&font_colour=000000&font_family=Lato&outline_colour=000000&coffee_colour=FFDD00'}
             alt='Buy me a coffee'
