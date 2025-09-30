2c300f04dda664684be9e5e9e8923af226e5bb6d
Aidan Weinberg <aidan3445@gmail.com>
2025-09-30 17:17:39 -0400

    delete league backedn func

--- src/app/api/leagues/[hash]/route.ts
+++ src/app/api/leagues/[hash]/route.ts
@@ -2,8 +2,9 @@ import 'server-only';
 
 import { type NextRequest, NextResponse } from 'next/server';
 import type { LeagueRouteParams } from '~/types/api';
-import { withLeagueMemberAuth } from '~/lib/apiMiddleware';
+import { withLeagueMemberAuth, withLeagueOwnerAuth } from '~/lib/apiMiddleware';
 import getLeague from '~/services/leagues/query/legaue';
+import deleteLeagueLogic from '~/services/leagues/mutation/deleteLeague';
 
 export async function GET(_: NextRequest, context: LeagueRouteParams) {
   return withLeagueMemberAuth(async (auth) => {
@@ -16,6 +17,18 @@ export async function GET(_: NextRequest, context: LeagueRouteParams) {
     } catch (e) {
       console.error(e);
       return NextResponse.json({ error: 'An error occurred while fetching the league.' }, { status: 500 });
+    }
+  })(context);
+}
+
+export async function DELETE(_: NextRequest, context: LeagueRouteParams) {
+  return withLeagueOwnerAuth(async (auth) => {
+    try {
+      const success = await deleteLeagueLogic(auth);
+      return NextResponse.json(success, { status: 200 });
+    } catch (e) {
+      console.error(e);
+      return NextResponse.json({ error: 'An error occurred while deleting the league.' }, { status: 500 });
     }
   })(context);
 }
