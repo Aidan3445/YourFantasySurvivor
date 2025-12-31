0c158ed59cc639c0238815b9db649c5cc70a1b49
Aidan Weinberg <aidan3445@gmail.com>
2025-12-31 10:36:57 -0600

    order spacing

--- src/components/leagues/predraft/order/view.tsx
+++ src/components/leagues/predraft/order/view.tsx
@@ -135,7 +135,7 @@ export default function DraftOrder({ overrideHash, scrollHeight, className }: Dr
                   <ColorRow
                     color={member.color}
                     loggedIn={leagueMembers.loggedIn?.memberId === member.memberId}>
-                    <h3 className='text-lg' style={{ color: getContrastingColor(member.color) }}>{index + 1}</h3>
+                    <h3 className='text-lg w-4' style={{ color: getContrastingColor(member.color) }}>{index + 1}</h3>
                     <h2 className='text-3xl font-semibold' style={{ color: getContrastingColor(member.color) }}>{member.displayName}</h2>
                     {!orderLocked &&
                       <GripVertical className='ml-auto cursor-row-resize' color={getContrastingColor(member.color)} />}
