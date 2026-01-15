1df890eec854fb35ced0c504eab827d3d2962417
Aidan Weinberg <aidan3445@gmail.com>
2026-01-15 15:18:22 -0600

    bottom nav colors

--- src/components/nav/bottom/view.tsx
+++ src/components/nav/bottom/view.tsx
@@ -1,21 +1,33 @@
 'use client';
 
 import { Flame, Trophy, BookUser } from 'lucide-react';
-import Image from 'next/image';
 import BottomNavUser from '~/components/nav/bottom/user';
 import BottomNavLink from '~/components/nav/bottom/link';
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+import { cn } from '~/lib/utils';
 
 export default function BottomNav() {
+  const pathname = usePathname();
+  const isHome = useMemo(() => pathname === '/', [pathname]);
+
   return (
     <div className='fixed bottom-0 left-0 right-0 bg-sidebar shadow-md z-50 md:hidden h-(--navbar-height)'>
       <span className='flex items-center justify-evenly h-full'>
         <BottomNavLink href='/'>
-          <Image
-            src='/Icon.ico'
-            alt='Your Fantasy Survivor Logo'
-            width={28}
-            height={28}
-            className='transition filter brightness-0 active:brightness-100 active:saturate-200 active:hue-rotate-90' />
+          <div
+            className={cn(
+              'size-7 transition-colors bg-[#674428] hover:bg-[#BCA46F] active:bg-[#99A275]',
+              isHome && 'bg-[#B58553]'
+            )}
+            style={{
+              maskImage: 'url(\'/Icon.ico\')',
+              maskSize: 'contain',
+              maskRepeat: 'no-repeat',
+              WebkitMaskImage: 'url(\'/Icon.ico\')',
+              WebkitMaskSize: 'contain',
+              WebkitMaskRepeat: 'no-repeat',
+            }} />
         </BottomNavLink>
         <BottomNavLink href='/seasons' icon={BookUser} />
         <BottomNavLink href='/playground' icon={Flame} />
