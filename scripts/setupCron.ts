94cfb3c7b513f22557e028f6d47dc27fed8d73d6
Aidan Weinberg <aidan3445@gmail.com>
2026-02-18 11:48:37 -0600

    fixing cron to se post

--- scripts/setupCron.ts
+++ scripts/setupCron.ts
@@ -1,11 +1,10 @@
 import { Client } from '@upstash/qstash';
-import { BASE_URL } from '~/lib/qStash';
 
 console.log('Setting up cron schedule for episodes', process.env.QSTASH_TOKEN, process.env.QSTASH_CRON_SECRET_KEY);
 const qstash = new Client({ token: process.env.QSTASH_TOKEN! });
 
 await qstash.schedules.create({
-  destination: `${BASE_URL}/api/notifications/episodes`,
+  destination: 'https://trialbyfiresurvivor.com/api/notifications/episodes',
   cron: '0 15 * * 6', // 15 UTC = 10am ET on Saturday
   headers: { authorization: `Bearer ${process.env.QSTASH_CRON_SECRET_KEY}` },
 });
